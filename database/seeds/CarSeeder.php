938686fbe6e454c5aa226b0f86cdd9b402c5aea3
Anders Jürisoo <jurisoo@hotmail.com>
2019-08-30 11:29:38 +0200

    Pipedream project files

--- database/seeds/CarSeeder.php
+++ database/seeds/CarSeeder.php
@@ -1,15 +1,17 @@
 <?php
 
+use App\Car;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CarSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
+        factory(Car::class, 10)->create();
     }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,15 +1,17 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
+        factory(User::class, 10)->create();
     }
 }
